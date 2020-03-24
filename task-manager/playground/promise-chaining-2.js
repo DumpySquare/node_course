c023b85913ec7b22b61a91cda4912bbce86973b3
Ben Gordon <bunookana@gmail.com>
2020-03-24 04:51:39 -0700

    3.23.2020 work

--- task-manager/playground/promise-chaining-2.js
+++ task-manager/playground/promise-chaining-2.js
@@ -5,11 +5,23 @@
 require('../src/db/mongoose')
 const Task = require('../src/models/task')
 
-Task.findByIdAndDelete('5e74ab535e343963f1560248').then((task) => {
-    console.log(task)
-    return Task.countDocuments({ completed: false })
-}).then((result) => {
-    console.log(result)
+// Task.findByIdAndDelete('5e74ab535e343963f1560248').then((task) => {
+//     console.log(task)
+//     return Task.countDocuments({ completed: false })
+// }).then((result) => {
+//     console.log(result)
+// }).catch((e) => {
+//     console.log(e)
+// })
+
+const deleteTaskAndCount = async (id) => {
+    const task = await Task.findByIdAndDelete(id)
+    const count = await Task.countDocuments({ completed: false })
+    return count
+}
+
+deleteTaskAndCount('5e73f92bcc827a1677eb5f64').then((count) => {
+    console.log(count)
 }).catch((e) => {
     console.log(e)
 })
---
+++ task-manager/src/routers/user.js
@@ -0,0 +1,87 @@
+const express = require('express')
+const User = require('../models/user')
+const router = new express.Router()
+
+// create user
+router.post('/users', async (req, res) => {
+    const user = new User(req.body)
+    try {
+        await user.save()
+        res.status(201).send(user)
+    } catch (e) {
+        res.status(400).send(e)
+    }
+})
+
+//get users
+router.get('/users', async (req, res) => {
+    try {
+        const users = await User.find({})
+        res.send(users)
+    } catch (e) {
+        res.status(500).send()
+    }
+})
+
+// get user by id
+router.get('/users/:id', async (req, res) => {
+    const _id = req.params.id
+    try {
+        const user = await User.findById(_id)
+        if (!user) {
+            return res.status(404).send()
+        }
+        res.send(user)
+    } catch (e) {
+        res.status(500).send()
+    }
+})
+
+router.patch('/users/:id', async (req, res) => {
+    // get request body to keyed list
+    const updates = Object.keys(req.body)
+    // set allowed updates fields
+    const allowedUpdates = ['name', 'email', 'password', 'age']
+    // compare update body to allowed fields above
+    const isValidOperation = updates.every((update) => allowedUpdates.includes(update))
+
+    // if field validation from above failed, respond with error
+    if (!isValidOperation) {
+        return res.status(400).send({ error: 'Invalid Update!' })
+    }
+
+    // valide update field, attemp to update user
+    try {
+
+        const user = await User.findById(req.params.id)
+        updates.forEach((update) => user[update] = req.body[update])
+        await user.save()
+
+        // try to find user by id and update, running data validators
+        // const user = await User.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true })
+        // if the update returned no users
+        if(!user) {
+            return res.status(404).send()
+        }
+        // found a user and updated, so return updated user data
+        res.send(user)
+    } catch (e) {
+        // something hrouterened with the query, respond with error
+        res.status(400).send(e, 'error from user patch')
+    }
+})
+
+// delete user
+router.delete('/users/:id', async (req, res) => {
+    try { 
+        const user = await User.findByIdAndDelete(req.params.id)
+        if (!user) {
+            return res.status(404).send()
+        }
+        res.send(user)
+    } catch (e) {
+        res.status(500).send()
+    }
+})
+
+module.exports = router
